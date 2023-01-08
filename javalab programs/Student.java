37feed5cb7dec8c80603ea02c7abf060a40b4ea5
Siddhanth P <siddhantprsd15@gmail.com>
2023-01-08 23:59:20 +0530

    Update Student.java

--- javalab programs/Student.java
+++ javalab programs/Student.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
+public class Student
+{
+String usn,name,branch;
+long phone;
+void insertStudent(String reg,String nm, String br,long ph)
+{
+usn=reg;
+name=nm;
+branch=br;
+phone=ph;
+}
+void displayStudent()
+{
+System.out.println("**********************");
+System.out.println("USN= "+usn);
+System.out.println("NAME= "+name);
+System.out.println("BRANCH= "+branch);
+System.out.println("PHONE NUMBER= "+phone);
+System.out.println("**********************");
+}
+public static void main(String args[])
+{
+Student st[]=new Student [100];
+Scanner ip=new Scanner(System.in);
+System.out.println("Enter the number of students");
+int n=ip.nextInt();
+for(int i=0;i<n;i++)
+st[i]=new Student();
+for(int j=0;j<n;j++)
+{
 
-public class Student {
-    public Student(String stuUSN, String stuName, String stuBranch, String stuPhone) {
-        System.out.println("Student USN is:" + stuUSN);
-        System.out.println("Student Name is:" + stuName);
-        System.out.println("Student Branch is:" + stuBranch);
-        System.out.println("Student Phone number is:" + stuPhone);
-    }
-
-    public static void main(String[] args) {
-        Scanner readInput = new Scanner(System.in);
-        System.out.println("Enter number of student objects to be created");
-        int numberOfStudents = readInput.nextInt();
-        for (int i = 1; i <= numberOfStudents; i++) {
-            System.out.println("Enter Student USN");
-            String usn = readInput.next();
-            System.out.println("Enter Student Name");
-            String name = readInput.next();
-            System.out.println("Enter Student Branch");
-            String branch = readInput.next();
-            System.out.println("Enter Student Phone");
-            String phone = readInput.next();
-            new Student(usn, name, branch, phone);
-        }
-    }
+System.out.println("Enter the Usn,Name,Branch,Phone Number");
+String usn=ip.next();
+String name=ip.next();
+String branch=ip.next();
+long phone=ip.nextLong();
+st[j].insertStudent(usn,name,branch,phone);
+}
+for( int m=0;m<n;m++)
+{
+System.out.format("Student %d details are\n",m+1);
+st[m].displayStudent();
+}
+}
 }
