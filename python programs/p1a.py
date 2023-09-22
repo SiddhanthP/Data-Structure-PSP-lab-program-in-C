a5ff9620e4546a520bf7e7c4ee73007b5c912f2b
Siddhanth P <siddhantprsd15@gmail.com>
2023-09-22 20:29:54 +0530

    Delete python programs directory

--- python programs/p1a.py
+++
@@ -1,8 +0,0 @@
-m1=int(input("enter marks of test 1\n"))
-m2=int(input("enter marks of test 2\n"))
-m3=int(input("enter marks of test 3\n"))
-
-minimum=min(m1,m2,m3)
-sum= (m1+m2+m3)-minimum
-avg=sum/2
-print("average marks of all three test is:",avg)
